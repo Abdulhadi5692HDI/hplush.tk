e674ef4452621f02e840b14f97b77c60179c3b21
Abdulhadi5692HDI <87948364+Abdulhadi5692HDI@users.noreply.github.com>
2022-06-20 19:34:51 +0100

    Website original source.
    Everything came from abdulhadishahzad.xp3.biz. Except for the TurboWarp Licesne

---
+++ form/_posts/2021-10-24-website-update.markdown
@@ -0,0 +1,11 @@
+---
+layout: post
+title:  "Website Update"
+date:   21-10-24
+categories: website update
+---
+
+## Abdulhadishahzad .xp3 .biz UPDATE!!!!!
+Welp, now this does not look like a plain html website.
+
+Now, introducting Announcements and Posts for updates of anything happening on this site
---
+++ js/cookies.js
@@ -0,0 +1,6 @@
+var downloads_cookie = "downloadpath=/dl/; expires=Mon, 01 Jan 2013 12:00:00 UTC; path=/dl";
+document.cookie = downloads_cookie;
+var main_cookie = "main=abdulhadishahzad.xp3.biz";
+document.cookie = main_cookie;
+var temp_html = "temp_html=PCFET0NUWVBFIGh0bWw+CjxodG1sPgo8Ym9keT4KPHA+VGVtcG9hcnkgTW9kZSE8L3A+CjwvYm9keT4KPC9odG1sPg==";
+document.cookie = temp_html
